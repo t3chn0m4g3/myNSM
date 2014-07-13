a406c0ad4aacb0819698c7aa0b6b76b35a43a63a
t3chn0m4g3 <marco.ochse@gmail.com>
2014-07-13 23:08:01 +0200

    Secure connection via SSL

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Suricata, Elastic Search, Kibana & Logstash          #
 # install script for Ubuntu server 14.04, x64          #
 #                                                      #
-# v0.34 by t3ChN0M4G3, 2014-07-01                      #
+# v0.36 by t3ChN0M4G3, 2014-07-13                      #
 ########################################################
 
 # Let's log for the beauty of it
@@ -17,14 +17,14 @@ myRED="tput setaf 1"
 myWHT="tput setaf 7"
 mySURICATAVERSION="2.0.2"
 myKIBANA="https://download.elasticsearch.org/kibana/kibana/kibana-3.1.0.tar.gz"
-myELASTIC="https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.deb"
+myELASTIC="https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.2.deb"
 myLOGSTASH="https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash_1.4.2-1-2c0f5a1_all.deb"
 
 # Let's make sure there is a warning if running for a second time
-#if [ -f install.log ];
-#  then $myRED; echo "### Running more than once may complicate things. Erase install.log if you are really sure."; $myWHT;
-#  exit 1;
-#fi
+if [ -f install.log ];
+  then $myRED; echo "### Running more than once may complicate things. Erase install.log if you are really sure."; $myWHT;
+  exit 1;
+fi
 
 # Let's ask for NIC and check existence
 while [ "$(ifconfig -s $myETH | grep $myETH -c)" != "1" ]
@@ -97,13 +97,13 @@ apt-get dist-upgrade -y
 
 # Let's install all the packages we need
 $myRED; echo "### Installing packages."; $myWHT
-apt-get install suricata oinkmaster ethtool apache2 apache2-utils openjdk-7-jdk openjdk-7-jre-headless -y
+apt-get install openssl suricata oinkmaster ethtool apache2 apache2-utils openjdk-7-jdk openjdk-7-jre-headless -y
 wget $myKIBANA
 wget $myELASTIC
 wget $myLOGSTASH
 tar -C /var/www/ -xzf kibana-3.1.0.tar.gz
 mv /var/www/kibana-3.1.0/ /var/www/kibana/
-dpkg -i elasticsearch-1.2.1.deb
+dpkg -i elasticsearch-1.2.2.deb
 dpkg -i logstash_1.4.2-1-2c0f5a1_all.deb
 
 # Check for supported suricata version
@@ -127,9 +127,6 @@ fi
 if [ -f /etc/suricata/suricata.yaml ]
   then mv /etc/suricata/suricata.yaml /etc/suricata/0ld/
 fi
-if [ -f /etc/oinkmaster.conf ]
-  then mv /etc/oinkmaster.conf /etc/oinkmaster.conf.backup
-fi
 if [ -f /etc/rc.local ]
   then mv /etc/rc.local /etc/rc.local.backup
 fi
@@ -146,7 +143,6 @@ chmod 644 /var/log/suricata/eve.json
 
 # Let's replace some files
 $myRED; echo "### Copying prepared configs to destination folders."; $myWHT
-cp oinkmaster.conf /etc/
 cp suricata.yaml /etc/suricata/
 cp myNSM.json /var/www/kibana/app/dashboards/default.json
 cp myNSM.json /var/www/kibana/app/dashboards/
@@ -162,6 +158,10 @@ $myRED; echo "### Creating test.rules."; $myWHT
 tee /etc/suricata/rules/test.rules <<EOF
 alert ip any any -> any any (msg:"ICMP detected"; sid:2; rev:1;)
 EOF
+
+# Let's patch oinkmaster.conf with update url
+$myRED; echo "### Patching oinkmaster.conf with update url."; $myWHT
+sed -i.bak 's#\# url = http://www.bleedingsnort.com/downloads/bleeding.rules.tar.gz#url = http://rules.emergingthreats.net/open/suricata/emerging.rules.tar.gz#' /etc/oinkmaster.conf
 
 # Let's pull some rules
 $myRED; echo "### Downloading latest rules."; $myWHT;
@@ -257,32 +257,71 @@ output {
 EOF
 
 # Let's prepare apache for a new site and auth-digest
-a2enmod auth_digest
+a2enmod auth_digest proxy proxy_http ssl
 mv myNSM.pwd /etc/apache2/
-tee /etc/apache2/sites-available/myNSM-site.conf <<EOF
-<VirtualHost *:80>
+tee /etc/apache2/sites-available/myNSM-ssl.conf <<EOF
+<IfModule mod_ssl.c>
+        <VirtualHost _default_:443>
+                ServerAdmin webmaster@localhost
+                DocumentRoot /var/www/kibana
 
-        ServerAdmin webmaster@localhost
-        DocumentRoot /var/www/kibana
+                ErrorLog /error.log
+                CustomLog /access.log combined
 
-<Directory /var/www/kibana>
-        AuthType Digest
-        AuthName "myNSM"
-        AuthDigestProvider file
-        AuthUserFile /etc/apache2/myNSM.pwd
-        Require valid-user
-        Options Indexes FollowSymLinks
-        AllowOverride None
-</Directory>
+                SSLEngine on
+                SSLCertificateFile      /etc/apache2/myNSM.crt
+                SSLCertificateKeyFile /etc/apache2/mySERVER.key
 
-        ErrorLog ${APACHE_LOG_DIR}/error.log
-        CustomLog ${APACHE_LOG_DIR}/access.log combined
+                SSLProtocol +TLSv1.2
+                SSLCompression off
+                SSLHonorCipherOrder on
+                SSLCipherSuite ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK
+                ProxyRequests off
+                ProxyPass /elasticsearch/ http://localhost:9200/
+                <Location /elasticsearch/>
+                        ProxyPassReverse /
+                        SSLRequireSSL
+                </Location>
 
-</VirtualHost>
+                <Directory /var/www/kibana>
+                        SSLRequireSSL
+                        AuthType Digest
+                        AuthName "myNSM"
+                        AuthDigestProvider file
+                        AuthUserFile /etc/apache2/myNSM.pwd
+                        Require valid-user
+                        Options Indexes FollowSymLinks
+                        AllowOverride None
+                </Directory>
+
+                <FilesMatch "\.(cgi|shtml|phtml|php)$">
+                        SSLOptions +StdEnvVars
+                </FilesMatch>
+                <Directory /usr/lib/cgi-bin>
+                        SSLOptions +StdEnvVars
+                </Directory>
+
+                BrowserMatch "MSIE [2-6]"                                 nokeepalive ssl-unclean-shutdown                                 downgrade-1.0 force-response-1.0
+                # MSIE 7 and newer should be able to use keepalive
+                BrowserMatch "MSIE [17-9]" ssl-unclean-shutdown
+
+        </VirtualHost>
+</IfModule>
 EOF
 sleep 3
-a2ensite myNSM-site.conf
+a2ensite myNSM-ssl.conf
 a2dissite 000-default.conf
+
+# Let's create the webserver certificates
+$myRED; echo "### Creating certificates."; $myWHT
+openssl genrsa -out mySERVER.key 8192
+openssl req -new -key mySERVER.key -out myNSM.csr
+openssl x509 -req -days 3650 -in myNSM.csr -signkey mySERVER.key -out myNSM.crt
+mv mySERVER.key myNSM.crt /etc/apache2/
+
+# Let's patch kibana config.js for ssl
+$myRED; echo "### Patching kibana config.js for ssl."; $myWHT
+sed -i.bak 's#elasticsearch: "http://"+window.location.hostname+":9200",#elasticsearch: "https://"+window.location.hostname+"/elasticsearch/",#' /var/www/kibana/config.js
 
 # Let's configure automatic start of services
 $myRED; echo "### Configuring automatic start of services."; $myWHT
@@ -294,6 +333,6 @@ service elasticsearch start
 service logstash start
 
 # Done
-$myRED; echo "### You can access kibana dashboard from your browser via http://your.ip"; $myWHT
+$myRED; echo "### You can access kibana dashboard from your browser via https://your.ip"; $myWHT
 $myRED; echo "### Done."; $myWHT
 exit 0
